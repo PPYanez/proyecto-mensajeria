8ee6daf9a37c6b7903072345d335d169ff883136
PPYanez <pedro.yanez@sansano.usm.cl>
2023-04-03 18:39:09 -0400

    feat: make the server accept connections, and handle them with separate threads.

---
+++ src/main/java/org/example/ClientHandler.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.net.Socket;
+
+public class ClientHandler {
+    public static void handleClient(Socket socket) {
+        // Handle the client
+        System.out.println("Client connected!");
+    }
+}
