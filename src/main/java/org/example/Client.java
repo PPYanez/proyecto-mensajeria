f2bd07d65f2284acfffcc9ede92a0a4abee28417
PPYanez <pedro.yanez@sansano.usm.cl>
2023-04-03 17:30:37 -0400

    Revert "Create server and client classes", it should'nt have been made on "main" branch
    This reverts commit 2c4b3908bf45cedc4ff3e0aed9dcc0ebcc5f1a48.

--- src/main/java/org/example/Client.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public class Client {
-    public static void main(String[] args) {
-    }
-}
--- src/main/java/org/example/Server.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Server {
-    public static void main(String[] args) {
-
-    }
-}
