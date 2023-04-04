f32894818df4a9306ce6432d98b0d0de1f1c63e0
PPYanez <pedro.yanez@sansano.usm.cl>
2023-04-04 11:12:23 -0400

    feat: make server read messages sent by the client

--- src/main/java/org/example/ClientHandler.java
+++ src/main/java/org/example/ClientHandler.java
@@ -1,10 +1,26 @@
 package org.example;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.Socket;
 
 public class ClientHandler {
     public static void handleClient(Socket socket) {
-        // Handle the client
-        System.out.println("Client connected!");
+        try{
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+
+            while(true){
+                String message = (String) ois.readObject();
+                if (message.equals("exit")) {
+                    break;
+                } else {
+                    System.out.println("Received message: " + message);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
