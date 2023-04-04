670547bb1a5e885eb2a4317c69bf1aab1c8597aa
PPYanez <pedro.yanez@sansano.usm.cl>
2023-04-03 20:09:15 -0400

    Revert "feat: read messages sent from the client"
    This reverts commit 86bacf7bb58575719fa05ef4f48a7df813d8d316.

--- src/main/java/org/example/ClientHandler.java
+++ src/main/java/org/example/ClientHandler.java
@@ -1,26 +1,10 @@
 package org.example;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.net.Socket;
 
 public class ClientHandler {
     public static void handleClient(Socket socket) {
-        while (true) {
-            try {
-                // Listen for messages from the client
-                ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-
-                String message = (String) ois.readObject();
-
-                if (message.equals("exit")) {
-                    break;
-                } else {
-                    System.out.println(message);
-                }
-            } catch (IOException | ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        // Handle the client
+        System.out.println("Client connected!");
     }
 }
