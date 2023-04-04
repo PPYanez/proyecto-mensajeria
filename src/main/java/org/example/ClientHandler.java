86bacf7bb58575719fa05ef4f48a7df813d8d316
PPYanez <pedro.yanez@sansano.usm.cl>
2023-04-03 20:06:30 -0400

    feat: read messages sent from the client

--- src/main/java/org/example/ClientHandler.java
+++ src/main/java/org/example/ClientHandler.java
@@ -1,10 +1,26 @@
 package org.example;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.Socket;
 
 public class ClientHandler {
     public static void handleClient(Socket socket) {
-        // Handle the client
-        System.out.println("Client connected!");
+        while (true) {
+            try {
+                // Listen for messages from the client
+                ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+
+                String message = (String) ois.readObject();
+
+                if (message.equals("exit")) {
+                    break;
+                } else {
+                    System.out.println(message);
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
