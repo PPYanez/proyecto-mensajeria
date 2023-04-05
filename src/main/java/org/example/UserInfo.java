d4ec1874fd846d8de8c130f9d2220bce57616e6e
PPYanez <pedro.yanez@sansano.usm.cl>
2023-04-05 11:49:04 -0400

    refactor: change UserInfo from record to class

--- src/main/java/org/example/UserInfo.java
+++ src/main/java/org/example/UserInfo.java
@@ -3,5 +3,20 @@ package org.example;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public record UserInfo(Socket userSocket, ObjectOutputStream oos) {
+public class UserInfo {
+    private final Socket userSocket;
+    private final ObjectOutputStream oos;
+
+    public UserInfo(Socket userSocket, ObjectOutputStream oos) {
+        this.userSocket = userSocket;
+        this.oos = oos;
+    }
+
+    public Socket getUserSocket() {
+        return userSocket;
+    }
+
+    public ObjectOutputStream getUserObjectOutputStream() {
+        return oos;
+    }
 }
