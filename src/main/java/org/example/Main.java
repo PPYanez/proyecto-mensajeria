96cc1c55508d25aa1fdc4ba9250d3aa7c6ec0941
pedro <pedro.yanez@sansano.usm.cl>
2023-04-08 18:28:24 -0400

    feat: scan lan to find if server is running

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import java.io.IOException;
+import java.net.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean serverUp = checkServerStatus(6666);
+    }
+
+    private static boolean checkServerStatus(int port) {
+        /**
+         * Apache's camel implementation to check if a server is up or not
+         * in the local network
+         * @param port the port to check for availability
+         */
+
+        boolean serverUp = true;
+
+        ServerSocket ss = null;
+        try {
+            ss = new ServerSocket(6666);
+            serverUp = false;
+        } catch (IOException e) {
+        } finally {
+            if (ss != null) {
+                try {
+                    ss.close();
+                } catch (IOException e) {
+                    // should not be thrown
+                }
+            }
+        }
+
+        return serverUp;
     }
 }
