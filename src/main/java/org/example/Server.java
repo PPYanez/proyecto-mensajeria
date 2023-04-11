387022ce19d5dd65b480c824a5d44abd5bcdc0fb
badsebita <lkjd743hlk@gmail.com>
2023-04-11 01:02:26 -0400

    feat: se agrega log de server, info: cliente conectado, warn: cliente intenta conectar, servidor lleno

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
@@ -10,8 +13,10 @@ public class Server {
     private static final int port = 6666;
     // Array that holds the clients
     public static ArrayList<UserInfo> clients = new ArrayList<>();
+    private static Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) throws IOException {
+
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
                 Socket socket = serverSocket.accept();
@@ -20,6 +25,7 @@ public class Server {
                 if (clients.size() < 2) {
                     // Set isServerFull flag
                     oos.writeObject(false);
+                    logger.info("Client connected.");
                     System.out.println("\u001B[31mClient connected.\u001B[0m");
 
                     UserInfo client = new UserInfo(socket, oos);
@@ -31,6 +37,7 @@ public class Server {
                 } else {
                     // Set isServerFull flag
                     oos.writeObject(true);
+                    logger.warn("Client trying to connect. Server is full.");
                     oos.close();
                     socket.close();
                 }
